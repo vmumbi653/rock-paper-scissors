ef02eeb44e009f98dceb9454101c1a0bfbe0b864
vmumbi653 <[mainamumbi23@gmail.com]>
2024-10-02 23:24:59 +0300

    Add increment value for player score variables.
    I have reset the humanScore and computerScore variable to have an increment of one point for each game played, where either the payer or the computer is the winner.

--- javascript.js
+++ javascript.js
@@ -24,23 +24,31 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     if (humanChoice==="rock" && computerChoice==="scissors") {
         alert("You win!  Rock beats scissors")
+        humanScore = humanScore+1;
     } else if (humanChoice==="paper" && computerChoice==="rock"){
         alert("You win! Paper beats rock")
+        humanScore = humanScore+1;
     } else if (humanChoice==="scissors" && computerChoice==="paper") {
         alert("You win! Scissors beats paper")
+        humanScore = humanScore+1;
     } else if(computerChoice==="rock" && humanChoice==="scissors") {
         alert("You lose! Rock beat scissors")
+        computerScore = computerScore+1;
     } else if(computerChoice==="paper" && humanChoice==="rock") {
         alert("You lose! Paper beats rock")
+        computerScore = computerScore+1;
     } else if(computerChoice==="scissors" && humanChoice==="paper") {
         alert("You lose! Scissors beats paper")
+        computerScore = computerScore+1;
     } else {
         alert("This is a draw!")
     }
+
 
 }
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
     playRound(humanSelection, computerSelection);
-
+    console.log(humanScore);
+    console.log(computerScore);
