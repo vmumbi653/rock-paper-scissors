ede20d34561dad925da85cd5aef4a17f73ca677f
vmumbi653 <[mainamumbi23@gmail.com]>
2024-10-02 11:04:24 +0300

    Add prompt box and use conditional statements.
    I have utilized the prompt method in the function getHumanChoice with a prompt message to prompt the user to input their choice. I have further declared a variable where the user's input will be stored.  I have used conditional statements to ensure that the function will return any of the valid choices that the user inputs.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,20 @@
 console.log("Hello Everybody");
 
+let choice = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    let choice=["rock", "paper", "scissors"];
-    return choice[Math.floor((Math.random()* choice.length))];
+    return choice[Math.floor((Math.random()* choice.length .toString()))];
 }
 console.log(getComputerChoice("rock", "paper", "scissors"));
+
+function getHumanChoice() {
+    let answer= prompt("Please enter your choice: ");
+    if (answer == "rock") {
+        return "rock";
+    } else if (answer== "paper") {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getHumanChoice("rock", "paper", "scissors"));
